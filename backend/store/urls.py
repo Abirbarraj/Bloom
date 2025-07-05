9677ec0223ed69c264bdec27336f569f073669d8
Abir barraj <abirbarraj224@gmail.com>
2025-07-05 13:52:32 +0100

    clear pages base

--- backend/store/urls.py
+++ backend/store/urls.py
@@ -2,8 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.error_page, name='home'),  # use 404.html as home
+    path('', views.home_page, name='home'),
     path('404/', views.error_page, name='error_page'),
+    path('about-us/', views.about_us, name='about_us'),
+    path('cart/', views.cart_variant, name='cart_variant'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contact/', views.contact_us, name='contact_us'),
+    path('faqs/', views.faqs, name='faqs'),
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register'),
+    path('product-count/', views.product_count_page, name='product_count'),
+    path('shop/', views.shop_grid, name='shop_grid'),
+    path('wishlist/', views.wishlist, name='wishlist'),
 ]
-
-
--- backend/store/views.py
+++ backend/store/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render
 
+def home_page(request):
+    return render(request, 'home.html')
+
+def about_us(request):
+    return render(request, 'about-us.html')
+
+def cart_variant(request):
+    return render(request, 'cart-variant2.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def contact_us(request):
+    return render(request, 'contact-us.html')
+
+def faqs(request):
+    return render(request, 'faqs.html')
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def register_page(request):
+    return render(request, 'register.html') 
+
+def product_count_page(request):
+    return render(request, 'product-visitor-sold-count.html')
+
+def shop_grid(request):
+    return render(request, 'shop-grid-3.html')
+
+def wishlist(request):
+    return render(request, 'wishlist.html')
+
 def error_page(request):
     return render(request, '404.html')
